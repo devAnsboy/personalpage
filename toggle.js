282d7ac3788da0db2d1edd11e91a5905cfd46c6d
Anselmo <anselmartins21@gmail.com>
2025-08-27 12:14:19 +0200

    fourthPhase

--- toggle.js
+++ toggle.js
@@ -1,16 +1,8 @@
 <script>
   const carousel = document.getElementById('carousel');
-  const nextBtn = document.getElementById('nextBtn');
-  const prevBtn = document.getElementById('prevBtn');
 
   // Scroll amount (match item width)
   const scrollAmount = 350;
-
-  nextBtn.addEventListener('click', () => {
-    carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-  });
-
-  prevBtn.addEventListener('click', () => {
-    carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  let scrollPosition = 0;
   });
 </script>
